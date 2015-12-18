7d94eba7a621bf5947038435dd827edf6750d77a
Mekayla <mdmcadoo@udel.edu>
2015-12-18 16:49:09 -0500

    Question C

---
+++ FinalExamQuestions/src/QuestionC/IllegalRectangle.java
@@ -0,0 +1,13 @@
+package QuestionC;
+
+public class IllegalRectangle extends Exception {
+	int x;
+	int y;
+	try{
+		
+		if( x <= 0 || y <= 0){
+			catch(IOException e){
+				system.out.println("X and Y cannot be negative numbers");
+			};
+		}
+}
